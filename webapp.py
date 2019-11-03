e69d1cc48d77cef52c93fffc20c0571f57ed4794
Juan Santana <jsantana21@users.noreply.github.com>
2019-11-03 13:50:31 -0500

    changed database to postgres.db

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ from forms import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cs460pa1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///postgres.db'
 db = SQLAlchemy(app)
 
 session = sessionmaker(bind=db)()
