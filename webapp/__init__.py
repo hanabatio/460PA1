cba622bc95068cdab745ff617fa3de1b1b7e5b81
Hana Batio <hbatio@bu.edu>
2019-11-04 22:18:49 -0500

    oops

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Junior7898@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Junior7898@localhost/postgres'
 db = SQLAlchemy(app)
 
 
